8f63f52893b6eb163ac8eb4f9452074a5a4cc9bf
Rupalibawaskar <mahajan.khushu@gmail.com>
2024-11-21 14:17:59 +0530

    Add files via upload

---
+++ Cafe management system.py
@@ -0,0 +1,48 @@
+#Define the menu of restaurant
+
+menu={
+    'Pizza':40,
+    'Pasta':50,
+    'Burger':60,
+    'Salad':70,
+    'Coffee':80,
+}
+
+#Greet
+print("welcome to masala magic Restaurant")
+print("Pizza: Rs40\nPasta: Rs50\nBurger:60 Rs\nSalad: Rs70\nCoffee: Rs80")
+
+
+order_total=0   #variable name
+#80+70=150
+
+
+
+item_1=input("Enter the name of item you want to order= ") #user input
+if item_1 in menu:
+    order_total += menu[item_1] #0+50
+    print(f"your item {item_1} has been added to your order")
+
+
+
+else:
+    print(f"Ordered item {item_1} is not available yet!")
+
+
+
+another_order= input("Do you want to add another item? (Yes/No) ")
+if another_order=="Yes":
+    item_2= input("Enter the name of second item =")
+    if item_2 in menu:
+        order_total+=menu[item_2]
+        print(f"Item {item_2} has been added to order")
+
+    else:
+          print(f"Ordered item {item_2} is not available!")
+
+print(f"The total amount of items to pay is {order_total}")          
+
+
+          
+        
+        
